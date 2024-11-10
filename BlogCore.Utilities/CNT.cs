a09db6e02e37f20ba0ecd42cb6bec3e9ea7663ce
JUAN RODRIGUEZ TABARES <Juan.RTabares@alumno.udg.mx>
2024-11-10 11:17:52 -0600

    Role System

---
+++ BlogCore.Utilities/CNT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.Utilities
+{
+    public class CNT
+    {
+        public const string Admin = "Admin";
+        public const string Register = "Register";
+        public const string Client = "Client";
+    }
+}
