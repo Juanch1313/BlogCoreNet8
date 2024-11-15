f2e030a8610d5066ea56f92ad24d5d38d85465a4
JUAN RODRIGUEZ TABARES <Juan.RTabares@alumno.udg.mx>
2024-11-14 22:09:34 -0600

    Users logic

---
+++ BlogCore/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using BlogCore.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public UsersController(IUnitOfWork unitOfWork,
+                                  IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Get all users
+            return View(_unitOfWork.userRepository.GetAll());
+        }
+
+        [HttpGet]
+        public IActionResult Block(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.userRepository.BlockUser(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Unlock(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.userRepository.UnlockUser(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
